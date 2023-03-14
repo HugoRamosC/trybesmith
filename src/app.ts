baf4ec904ecb8976f390ba0dd9a42c1bdcad86a2
HugoRamosC <hugor.castilho@gmail.com>
2023-03-14 17:28:42 -0300

    start: feat post produtcs endpoint

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,14 @@
 import express from 'express';
+import productController from './controllers/product.controller';
 
 const app = express();
 
 app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.status(200).send('Up and running');
+});
+
+app.post('/products', productController.createProduct);
 
 export default app;
