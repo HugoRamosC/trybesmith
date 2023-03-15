03afb2e7989a8aac7a0f217622b5da6af5814c52
HugoRamosC <hugor.castilho@gmail.com>
2023-03-15 11:38:31 -0300

    refact: login input validations with joi

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,6 +1,7 @@
 import { IError, ILogin, IUser } from '../interfaces';
 import userModel from '../models/user.model';
 import { generateToken } from '../utils/auth';
+import { validateLogin } from './validation/validations';
 
 const createUser = async (user: IUser): Promise<string> => {
   const { id, username } = await userModel.createUser(user);
@@ -9,12 +10,12 @@ const createUser = async (user: IUser): Promise<string> => {
 };
 
 const login = async (inputLogin: ILogin): Promise<IError> => {
-  const { username, password } = inputLogin;
-  if (!username) return { status: 400, message: '"username" is required' };
-  if (!password) return { status: 400, message: '"password" is required' };
+  const validated = validateLogin(inputLogin);
+  if (validated.error) return { status: 400, message: validated.error.message };
   
+  const { username, password } = inputLogin;
   const user = await userModel.login(inputLogin);
-  if (user.length === 0 || user[0].password !== inputLogin.password) {
+  if (user.length === 0 || user[0].password !== password) {
     return { status: 401, message: 'Username or password invalid' };
   }
 
